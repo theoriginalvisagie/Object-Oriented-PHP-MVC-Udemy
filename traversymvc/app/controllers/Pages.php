dd5f6b6185a87a53b567273234407dfe853df6f6
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-02-22 19:40:51 +0200

    Clean up

--- traversymvc/app/controllers/Pages.php
+++ traversymvc/app/controllers/Pages.php
@@ -1,12 +1,11 @@
 <?php
     class Pages extends Controller{
+
         public function __construct(){
-            // echo "Pages Loaded";
-            $this->postModel = $this->model("Post");
+
         }
 
         public function index(){
-            // echo "PAGES INDEX";
             $data = array("tile"=>"Welcome");
             $this->view("pages/index",$data);
         }
