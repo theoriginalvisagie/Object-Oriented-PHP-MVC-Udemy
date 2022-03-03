c0385e44fcaced3b5f10215555d3f31a778130bd
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-03-03 20:27:55 +0200

    fixes

--- shareposts/app/helpers/session_helper.php
+++ shareposts/app/helpers/session_helper.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    //Flash MEssage Helper
+    //Flash Message Helper
     function flash($name = "", $message = "", $class = "alert alert-success"){
         if(!empty($name)){
             if(!empty($message) && empty($_SESSION['name'])){
@@ -12,9 +12,10 @@
                 if(!empty($_SESSION[$name."_class"])){
                     unset($_SESSION[$name."_class"]);
                 }
+
                 $_SESSION[$name] = $message;
                 $_SESSION[$name."_class"] = $class;
-            }else if(empty($message) && !empty($_SESSION['name'])){
+            }else if(empty($message) && !empty($_SESSION[$name])){
                 $class = !empty($_SESSION[$name."_class"]) ? $_SESSION[$name."_class"] : "";
                 echo "<div class='$class' id='msg-flash'>{$_SESSION[$name]}</div>";
                 unset($_SESSION[$name]);
--- shareposts/app/views/users/login.php
+++ shareposts/app/views/users/login.php
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-md-6 mx-auto">
       <div class="card card-body bg-light mt-5">
-        <?php echo flash("register_success");?>
+        <?php echo  flash("register_success");?>
         <h2>Login</h2>
         <p>Please fill in your credentials to log in</p>
         <form action="<?php echo URLROOT; ?>/users/login" method="post">
@@ -28,4 +28,6 @@
       </div>
     </div>
   </div>
-<?php require APPROOT . '/views/inc/footer.php'; ?>
+<?php require APPROOT . '/views/inc/footer.php'; 
+  echo "<pre>".print_r($_SESSION,true)."</pre>";
+?>
